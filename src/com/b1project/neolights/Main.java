a766d805e9b699fcedc232a8a611c82535d4301c
cbosselut <bossone0013@gmail.com>
2018-02-08 16:03:25 +0100

    - add dbus interface - add benchmark mode

--- src/com/b1project/neolights/Main.java
+++ src/com/b1project/neolights/Main.java
@@ -1,10 +1,13 @@
 package com.b1project.neolights;
 
+import com.b1project.neolights.interfaces.DBusLightsInterface;
 import dorkbox.systemTray.SystemTray;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacv.FFmpegFrameGrabber;
 import org.bytedeco.javacv.FrameGrabber;
 import org.bytedeco.javacv.OpenCVFrameConverter;
+import org.freedesktop.dbus.DBusConnection;
+import org.freedesktop.dbus.exceptions.DBusException;
 
 import javax.swing.*;
 import java.awt.*;
@@ -30,7 +33,7 @@ import java.net.Socket;
  * You should have received a copy of the Apache License
  * along with this program.  If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
  */
-class Main {
+class Main implements DBusLightsInterface {
 
     private final static String DEFAULT_HOST_ADDRESS = "192.168.7.2";
     private final static int DEFAULT_HOST_PORT = 45045;
@@ -40,9 +43,13 @@ class Main {
     private final static int DISPLAY_WIDTH = 1920;
     private final static int DISPLAY_HEIGHT = 1080;
     private final static int SAMPLE_HEIGHT = 200;
+    private final static int DEFAULT_TEST_PASS = 500;
 
     private static Socket socket;
     private static PrintWriter outPrintWriter;
+    private static int r_default = 0x77;
+    private static int g_default = 0;
+    private static int b_default = 0;
     private static int r_top_old = 0;
     private static int g_top_old = 0;
     private static int b_top_old = 0;
@@ -51,6 +58,7 @@ class Main {
     private static int bottom_b_old = 0;
     private static boolean notification_visible = false;
     private static boolean pause_grabber = false;
+    private static boolean should_quit = false;
     private static String host_address = DEFAULT_HOST_ADDRESS;
     private static int host_port = DEFAULT_HOST_PORT;
     private final static AppIcon APP_ICON = new AppIcon("/icon.png", "Application icon");
@@ -63,23 +71,73 @@ class Main {
         host_port = Integer.parseInt(args[1]);
         SwingUtilities.invokeLater(Main::showTrayIcon);
         openSocket();
+        new Main().start();
+    }
+
+    private void start() {
+        try {
+            DBusConnection dbusConnection = DBusConnection.getConnection(DBusConnection.SESSION);
+            dbusConnection.requestBusName("com.b1project.neolights.Main");
+            dbusConnection.exportObject("/com/b1project/neolights/Main", this);
+            while (!should_quit) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ignored) {}
+            }
+            System.out.println("should_quit: " + should_quit);
+            dbusConnection.disconnect();
+        }
+        catch (DBusException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void exit() {
         try {
+            should_quit = true;
             pause_grabber = true;
-            if(socket == null) {
-                openSocket();
+            if(socket != null) {
+                Thread.sleep(200);
+                sendColorValue(0, 0, 0, 0, 0, 0);
+                Thread.sleep(2);
             }
-            Thread.sleep(200);
-            sendColorValue(0, 0, 0, 0, 0, 0);
-            Thread.sleep(2);
             System.exit(0);
 
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             System.err.println("\nError: " + e.getMessage());
         }
+    }
+
+    @Override
+    public boolean getGrabberStatus() {
+        return !pause_grabber;
+    }
+
+    @Override
+    public boolean toggleGrabber() {
+        try {
+            set_grabber_status(!pause_grabber);
+        }
+        catch (Exception ignore){}
+        return getGrabberStatus();
+    }
+
+    @Override
+    public void quitGrabber() {
+        exit();
+    }
+
+    @Override
+    public int[] getStandbyColor() {
+        return new int[]{r_default, g_default, b_default};
+    }
+
+    @Override
+    public void setStandbyColor(int red, int green, int blue) {
+        r_default = red;
+        g_default = green;
+        b_default = blue;
     }
 
     private static void quit() {
@@ -148,7 +206,147 @@ class Main {
                     JOptionPane.INFORMATION_MESSAGE,
                     APP_ICON.getIcon());
         });
+        tray.addMenuEntry("Benchmark", (systemTray, menuEntry) -> benchmark());
         tray.addMenuEntry("Quit", (systemTray, menuEntry) -> exit());
+    }
+
+    private static void benchmark() {
+        boolean grabber_status = pause_grabber;
+        if(!pause_grabber) {
+            set_grabber_status(true);
+        }
+        FFmpegFrameGrabber topGrabber = null;
+        FFmpegFrameGrabber bottomGrabber = null;
+        long total_time_ms = 0;
+        long duration;
+        System.out.println("\nStart benchmark");
+        try {
+            DisplayInfo displayInfo = new DisplayInfo();
+            OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+            int top_r = 0;
+            int top_g = 0;
+            int top_b = 0;
+            int bottom_r = 0;
+            int bottom_g = 0;
+            int bottom_b = 0;
+            int pass = 0;
+
+            int bottom_y = DISPLAY_HEIGHT + DISPLAY_OFFSET_Y - SAMPLE_HEIGHT;
+            topGrabber = new FFmpegFrameGrabber(":0.0+" + DISPLAY_OFFSET_X + "," + DISPLAY_OFFSET_Y);
+            topGrabber.setFormat("x11grab");
+            topGrabber.setImageWidth(DISPLAY_WIDTH);
+            topGrabber.setImageHeight(SAMPLE_HEIGHT);
+            topGrabber.start();
+
+            bottomGrabber = new FFmpegFrameGrabber(":0.0+" + DISPLAY_OFFSET_X + "," + bottom_y);
+            bottomGrabber.setFormat("x11grab");
+            bottomGrabber.setImageWidth(DISPLAY_WIDTH);
+            bottomGrabber.setImageHeight(SAMPLE_HEIGHT);
+            bottomGrabber.start();
+            int max_pass = DEFAULT_TEST_PASS;
+            while(pass < max_pass){
+                if(displayInfo.getDisplayStatus() != 0) {
+                    System.err.println("\nScreen is not awake");
+                    break;
+                }
+                long start = System.currentTimeMillis();
+                final opencv_core.IplImage top_screenshot = converter.convert(topGrabber.grab());
+                final opencv_core.IplImage bottom_screenshot = converter.convert(bottomGrabber.grab());
+
+                for (int i = 0; i < DISPLAY_WIDTH; i = i + 4) {
+                    for (int j = 0; j < SAMPLE_HEIGHT; j = j + 4) {
+                        opencv_core.CvScalar top_scalar = opencv_core.cvGet2D(top_screenshot, j, i);
+                        opencv_core.CvScalar bottom_scalar = opencv_core.cvGet2D(bottom_screenshot, j, i);
+                        top_r += (int) top_scalar.val(2);
+                        top_g += (int) top_scalar.val(1);
+                        top_b += (int) top_scalar.val(0);
+                        bottom_r += (int) bottom_scalar.val(2);
+                        bottom_g += (int) bottom_scalar.val(1);
+                        bottom_b += (int) bottom_scalar.val(0);
+                    }
+                }
+                top_r = Math.round(top_r / (480 * 50)); //average red
+                top_g = Math.round(top_g / (480 * 50)); //average green
+                top_b = Math.round(top_b / (480 * 50)); //average blue
+                bottom_r = Math.round(bottom_r / (480 * 50)); //average red
+                bottom_g = Math.round(bottom_g / (480 * 50)); //average green
+                bottom_b = Math.round(bottom_b / (480 * 50)); //average blue
+                duration = System.currentTimeMillis() - start;
+                total_time_ms += duration;
+                pass += 1;
+                if(pass % (5 * max_pass / 100) == 0){
+                    System.out.print('█');
+                }
+            }
+            show_benchmark_result(pass, total_time_ms);
+        }
+        catch (FrameGrabber.Exception e) {
+            System.err.println("\nGrabber fails");
+        }
+        finally {
+            if(topGrabber != null) {
+                try {
+                    topGrabber.stop();
+                    topGrabber.release();
+                } catch (FrameGrabber.Exception e) {
+                    System.err.println("\nCan't stop top grabber");
+                }
+            }
+            if(bottomGrabber != null) {
+                try {
+                    bottomGrabber.stop();
+                    bottomGrabber.release();
+                } catch (FrameGrabber.Exception e) {
+                    System.err.println("\nCan't stop bottom grabber");
+                }
+            }
+            System.out.println("\nBenchmark complete");
+            if(!grabber_status) {
+                set_grabber_status(false);
+            }
+        }
+    }
+
+    private static void show_benchmark_result(int pass, long total_time_ms){
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.weightx = 1.0f;
+        constraints.weighty = 1.0f;
+        constraints.insets = new Insets(5, 5, 5, 5);
+        constraints.fill = GridBagConstraints.BOTH;
+        double total_time = (double) total_time_ms / 1000;
+        JPanel panel = new JPanel(new GridBagLayout());
+        JLabel benchmarkResultLabel =
+                new JLabel(
+                        "<html><span style='font-weight:bold;font-size:1.52em'>Total time: " + total_time + "s<span></html>"
+                );
+        panel.add(benchmarkResultLabel, constraints);
+        constraints.gridy++;
+        double average = total_time / pass;
+        JLabel benchmarkAverageLabel =
+                new JLabel(
+                        "<html><span>Average: " + average + "</span></html>"
+                );
+        panel.add(benchmarkAverageLabel, constraints);
+        constraints.gridy++;
+        double fps = pass / total_time;
+        JLabel benchmarkFPSLabel =
+                new JLabel(
+                        "<html><span>Frame/s: " + fps + "</span></html>"
+                );
+        panel.add(benchmarkFPSLabel, constraints);
+        constraints.gridy++;
+        JLabel benchmarkPassLabel =
+                new JLabel(
+                        "<html><span>Pass: " + pass + "</span></html>"
+                );
+        panel.add(benchmarkPassLabel, constraints);
+        JOptionPane.showMessageDialog(null,
+                panel,
+                "Benchmark Results",
+                JOptionPane.INFORMATION_MESSAGE,
+                APP_ICON.getIcon());
     }
 
     private static void scan_display(){
@@ -181,7 +379,7 @@ class Main {
             //noinspection InfiniteLoopStatement
             while(true) {
                 if(pause_grabber){
-                    System.out.println("\n Grabber in standby");
+                    System.out.println("\nGrabber in standby");
                     Thread.sleep((int)(displayInfo.getRefreshRate() * 7.5));
                     break;
                 }
@@ -189,7 +387,7 @@ class Main {
                     ask_for_connection();
                     break;
                 }
-                System.out.printf("  Scanning %s                          \r", loader);
+                System.out.printf("    Scanning %s                          \r", loader);
                 switch (loader){
                     case "/":
                         loader = "-";
@@ -236,12 +434,8 @@ class Main {
                     bottom_g = 0x00;
                     bottom_b = 0x00;
                 }
-                if(top_r != r_top_old || top_g != g_top_old || top_b != b_top_old
-                        || bottom_r != bottom_r_old || bottom_g != bottom_g_old || bottom_b != bottom_b_old) {
-                    System.out.printf("  Changes detected, sending request...\r");
-                    sendColorValue(top_r, top_g, top_b, bottom_r, bottom_g, bottom_b);
-                }
-                Thread.sleep((int)(displayInfo.getRefreshRate() * 7.5));
+                process_scan_result(top_r, top_g, top_b, bottom_r, bottom_g, bottom_b);
+                Thread.sleep((int)(displayInfo.getRefreshRate() * 2.0));
             }
         }
         catch (InterruptedException e){
@@ -271,26 +465,56 @@ class Main {
         }
     }
 
+    private static void process_scan_result(int top_r, int top_g, int top_b,
+                                       int bottom_r, int bottom_g, int bottom_b){
+        if(top_r != r_top_old || top_g != g_top_old || top_b != b_top_old
+                || bottom_r != bottom_r_old || bottom_g != bottom_g_old || bottom_b != bottom_b_old) {
+            System.out.print("    Changes detected, sending request...\r");
+            sendColorValue(top_r, top_g, top_b, bottom_r, bottom_g, bottom_b);
+            r_top_old = top_r;
+            g_top_old = top_g;
+            b_top_old = top_b;
+            bottom_r_old = bottom_r;
+            bottom_g_old = bottom_g;
+            bottom_b_old = bottom_b;
+        }
+    }
+
     private static void set_grabber_status(boolean status){
         final SystemTray tray = SystemTray.getSystemTray();
         pause_grabber = status;
         if(!pause_grabber ){
-            tray.updateMenuEntry_Text("Run grabber", "Pause grabber");
-            tray.setIcon(APP_ICON.getURL());
-            openSocket();
+            try {
+                tray.updateMenuEntry_Text("Run grabber", "Pause grabber");
+                tray.setIcon(APP_ICON.getURL());
+            }
+            catch (NullPointerException e){
+                System.err.println("\nError: can't update tray icon menu");
+                System.err.println("\nError: " + e.getMessage());
+            }
+            finally {
+                openSocket();
+            }
         }
         else{
-            tray.updateMenuEntry_Text("Pause grabber", "Run grabber");
-            tray.setIcon(APP_ICON_OFF.getURL());
             try {
-                sendColorValue(0x77, 0, 0, 0x77, 0, 0);
-                Thread.sleep(2);
-                closeSocket();
+                tray.updateMenuEntry_Text("Pause grabber", "Run grabber");
+                tray.setIcon(APP_ICON_OFF.getURL());
             }
-            catch (Exception e) {
+            catch (NullPointerException e){
+                System.err.println("\nError: can't update tray icon menu");
                 System.err.println("\nError: " + e.getMessage());
-                ask_for_connection();
+            }
+            finally {
+                try {
+                    sendColorValue(r_default, g_default, b_default, r_default, g_default, b_default);
+                    Thread.sleep(2);
+                    closeSocket();
+                } catch (Exception e) {
+                    System.err.println("\nError: " + e.getMessage());
+                    ask_for_connection();
 
+                }
             }
         }
     }
@@ -305,12 +529,6 @@ class Main {
         );
         Thread t = new Thread(() -> sendRequest(request));
         t.start();
-        r_top_old = top_r;
-        g_top_old = top_g;
-        b_top_old = top_b;
-        bottom_r_old = bottom_r;
-        bottom_g_old = bottom_g;
-        bottom_b_old = bottom_b;
     }
 
     private static void openSocket(){
@@ -361,7 +579,9 @@ class Main {
             }
             else {
                 outPrintWriter.println(request);
-                System.out.printf("  Done                                \r");
+                if(!pause_grabber) {
+                    System.out.print("    Done                                \r");
+                }
             }
         }
         else{
@@ -460,5 +680,15 @@ class Main {
         /*while (notification_visible){
             // wait
         }*/
+    }
+
+    @Override
+    public boolean isRemote() {
+        return false;
+    }
+
+    @Override
+    public String getObjectPath() {
+        return null;
     }
 }
---
+++ src/com/b1project/neolights/interfaces/DBusLightsInterface.java
@@ -0,0 +1,40 @@
+package com.b1project.neolights.interfaces;
+
+import org.freedesktop.DBus;
+import org.freedesktop.dbus.DBusInterface;
+
+/**
+ * Copyright (C) 2018 Cyril Bosselut <bossone0013@gmail.com>
+ * <p>
+ * This file is part of NeoLights
+ * <p>
+ * NeoLights is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This libraries are distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+public interface DBusLightsInterface extends DBusInterface {
+
+    @DBus.Description("Get grabber status")
+    boolean getGrabberStatus();
+
+    @DBus.Description("Toggle grabber status")
+    boolean toggleGrabber();
+
+    @DBus.Description("Terminate the application")
+    void quitGrabber();
+
+    @DBus.Description("Get standby color")
+    int[] getStandbyColor();
+
+    @DBus.Description("Set standby color")
+    void setStandbyColor(int red, int green, int blue);
+}
